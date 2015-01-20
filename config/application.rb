9d158c8218dde9b7794c8bfd6a9f6f92982f9f64
leomdivani <leomdiv@gmail.com>
2015-01-20 18:34:14 +0000

    ah

--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,10 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-config.serve_static_assets = true
 Bundler.require(:default, Rails.env)
 module Milk
   class Application < Rails::Application
+    config.serve_static_assets = true
+    
   end
 end
