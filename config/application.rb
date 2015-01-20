6016fd091324182942d42e187f543f256d212400
leomdivani <leomdiv@gmail.com>
2015-01-20 18:37:13 +0000

    hk

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,7 @@ require 'rails/all'
 Bundler.require(:default, Rails.env)
 module Milk
   class Application < Rails::Application
-    config.serve_static_assets = true
-    
+    config.assets.compile = true
+    config.assets.precompile =  ['*.js', '*.css', '*.css.erb']
   end
 end
