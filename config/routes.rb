05c0b0d3c9de522328ff5415ddbc1e3d66ca709a
leomdivani <leomdiv@gmail.com>
2015-01-21 15:36:05 +0000

    devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Milk::Application.routes.draw do
-  get "home/index"
-  root 'home#index'
 
+  get "home/index"
+
+  root 'home#index'
+  devise_for :users
+
+  get 'client_portal' => 'client_portal#index'
 end
---
+++ test/controllers/client_portal_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ClientPortalControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
