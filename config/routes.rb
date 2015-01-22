dc5f7efbc7730ff599553654e0efa4bb95854133
leomdivani <leomdiv@gmail.com>
2015-01-22 20:08:06 +0000

    routes for links

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,15 @@ Milk::Application.routes.draw do
 
   root 'home#index'
   devise_for :users, :controllers => {sessions: 'sessions', registrations: 'registrations'}
-  
+
   get 'client_portal' => 'client_portal#index'
+  get "client_portal/company_description" => "client_portal#company_description", as: :company_description
+  get "client_portal/futures" => "client_portal#futures", as: :futures
+  get "client_portal/primary_object" => "client_portal#primary_object", as: :investment
+  get "client_portal/investment" => "client_portal#investment", as: :investment
+  get "client_portal/riot_ritual" => "client_portal#riot_ritual", as: :riot_ritual
+
+
 
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
